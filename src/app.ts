07c67378e6b05640476f8f54650187134092f93b
Md Moniruzzaman <mdmoniruzzamanshuvo2003@gmail.com>
2025-12-10 15:30:54 +0600

    change vercel cli file

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,53 @@
+// /* eslint-disable no-undef */
+// /* eslint-disable no-unused-vars */
+// /* eslint-disable @typescript-eslint/no-unused-vars */
+// /* eslint-disable @typescript-eslint/no-explicit-any */
+// import cookieParser from 'cookie-parser';
+// import cors from 'cors';
+// import express, { Application, Request, Response } from 'express';
+// import notFound from './app/middlewares/notFound.js';
+// import router from './app/routes/index.js';
+// import { createServer } from 'http';
+// import helmet from 'helmet';
+// import globalErrorHandler from './app/middlewares/globalErrorhandler.js';
+// const app: Application = express();
+// const httpServer = createServer(app);
+
+// // Middleware
+// app.use(helmet()); // Security headers
+// app.use(express.json({ limit: '50mb' }));
+// app.use(cookieParser());
+// app.use(express.urlencoded({ limit: '50mb', extended: true }));
+// app.use(
+//   cors({
+//     origin:"*",
+//     credentials: true,
+//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+//     // origin: ['http://192.168.12.63:5173', 'http://192.168.12.63:3001', 'http://34.233.41.57:3000', 'http://34.233.41.57:3001',],
+//   })
+// );
+
+// app.use(express.json({ verify: (req: any, res, buf) => { req.rawBody = buf.toString(); } }));
+
+// // Routes
+// app.use('/api/v1', router);
+
+// app.get('/', (req: Request, res: Response) => {
+//   res.send('Welcome To Project-Management API !');
+// });
+
+// app.use(globalErrorHandler);
+// app.use(notFound);
+
+// process.on('SIGTERM', () => {
+//   // eslint-disable-next-line no-console
+//   console.log('SIGTERM received: closing server...');
+//   // eslint-disable-next-line no-console
+//   httpServer.close(() => console.log('Server closed.'));
+// });
+
+// export default httpServer;
+
 /* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
 /* eslint-disable @typescript-eslint/no-unused-vars */
@@ -10,8 +60,9 @@ import router from './app/routes/index.js';
 import { createServer } from 'http';
 import helmet from 'helmet';
 import globalErrorHandler from './app/middlewares/globalErrorhandler.js';
+
 const app: Application = express();
-const httpServer = createServer(app);
+const httpServer = createServer(app); // Vercel ignores this for deployment, but keeps it for local use
 
 // Middleware
 app.use(helmet()); // Security headers
@@ -20,17 +71,20 @@ app.use(cookieParser());
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(
   cors({
-    origin:"*",
+    origin: '*',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     // origin: ['http://192.168.12.63:5173', 'http://192.168.12.63:3001', 'http://34.233.41.57:3000', 'http://34.233.41.57:3001',],
-  })
+  }),
 );
 
-app.use(express.json({ verify: (req: any, res, buf) => { req.rawBody = buf.toString(); } }));
-
-
-
+app.use(
+  express.json({
+    verify: (req: any, res, buf) => {
+      req.rawBody = buf.toString();
+    },
+  }),
+);
 
 // Routes
 app.use('/api/v1', router);
@@ -49,4 +103,6 @@ process.on('SIGTERM', () => {
   httpServer.close(() => console.log('Server closed.'));
 });
 
-export default httpServer;
+// **--- THE FIX ---**
+// Change the default export to the Express app instance (app)
+export default app;
